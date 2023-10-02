f7431cc7aed429074328f3578c8b0f694ab0f6bc
zhuyue2541 <zhuyue2541@outlook.com>
2023-10-02 20:41:04 +0800

    增加确认订单按钮

--- miniprogram/pages/messages/messages.ts
+++ miniprogram/pages/messages/messages.ts
@@ -13,10 +13,25 @@ Page({
     //我发布的，未被锁定
     myPublish: [{ name: "书桌", publishTime:"2023.10.01",buyOrSell: "出售", lockTime: "2023.10.01", endTime: "2023.10.10", publisher:{wxName:"zhuyue1242",telNo:"123465421fe654",position:"F区10号楼，发布"},locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼锁定" }, id: 1, description: "阿飞发生阿飞阿飞阿飞阿飞阿飞阿飞", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", publisher:{wxName:"zhuyue1242",telNo:"123465421fe654",position:"F区10号楼"},endTime: "2023.10.10", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 2, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 3, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 4, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" },{ name: "书桌", buyOrSell: "出售", lockTime: "2023.10.01", endTime: "2023.10.10", publisher:{wxName:"zhuyue1242",telNo:"123465421fe654",position:"F区10号楼，发布"},locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼锁定" }, id: 1, description: "阿飞发生阿飞阿飞阿飞阿飞阿飞阿飞", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", publisher:{wxName:"zhuyue1242",telNo:"123465421fe654",position:"F区10号楼"},endTime: "2023.10.10", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 2, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 3, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" }, { name: "手机", buyOrSell: "出售", locker: { wxName: "zhuyue1242", telNo: "123465421fe654", position: "F区10号楼" }, id: 4, description: "", "classify": "其他", image: "/images/product/OIP-C.jpg" }]
   },
-  showConfirm: function () {
+  confirm: function(){
     wx.showModal({
-      title: '确认取消',
-      content: '确定要取消锁定吗？',
+      title: '确认完成',
+      content: '确定订单完成吗？',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+          // 在这里执行取消操作
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+          // 在这里执行取消操作
+        }
+      }
+    })
+  },
+  unlock: function () {
+    wx.showModal({
+      title: '确认解除锁定',
+      content: '确定要取消锁定吗？解锁后其他人可以锁定。',
       success: function (res) {
         if (res.confirm) {
           console.log('用户点击确定');
