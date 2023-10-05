184b4c5824bf1c08b77d995aee3d4a536fade289
zhuyue2541 <zhuyue2541@outlook.com>
2023-10-05 10:34:38 +0800

    申请记录审批完成

--- miniprogram/pages/contact/contact.ts
+++ miniprogram/pages/contact/contact.ts
@@ -5,11 +5,85 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isAdministrator: true,
     myCommunity: ["雅居乐1", "智慧城2", "雅居乐3", "智慧城4", "雅居乐5", "智慧城6"],
-    myContacts:["电话：12093，微信：239ae3,A区，3单元3号楼","F楼"]
+    applications: [{ OpenId: "219039", nickName: "zzhh啊", community: "雅居乐", description: "F区10号楼" }, { OpenId: "219034", nickName: "zhh啊", community: "雅居乐", description: "F区10号楼" }, { OpenId: "21ae90", nickName: "hh啊", community: "雅居乐", description: "F区10号楼" }],
+    myContacts: ["电话：12093，微信：239ae3,A区，3单元3号楼", "F楼"]
   },
-  deletOneContact(e) {    
-    if(this.data.myContacts.length == 1){
+  confirmAccess(e) {
+    let index = e.currentTarget.dataset.index;
+    var applications = [];
+    console.log(e);
+    this.data.applications.forEach((item, _) => {
+      if (item.OpenId != index) {
+        applications.push(item);
+      }
+    });
+    this.setData({
+      applications
+    })
+  },
+  access(e) {
+    let that = this;
+    let index = e.currentTarget.dataset.index;
+    this.data.applications.forEach((item, _) => {
+      if (item.OpenId == index) {        
+        wx.showModal({
+          title: '确认通过已下申请吗',
+          content: item.nickName +'申请' + item.community,
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              //发送后端 TODO
+              that.confirmAccess(e);              
+            } else if (res.cancel) {
+              console.log('用户点击取消');
+              // 在这里执行取消操作
+            }
+          }
+        })
+        return;
+      }
+    });
+  },
+  confirmRefuse(e) {
+    let index = e.currentTarget.dataset.index;
+    var applications = [];
+    console.log(e);
+    this.data.applications.forEach((item, _) => {
+      if (item.OpenId != index) {
+        applications.push(item);
+      }
+    });
+    this.setData({
+      applications
+    })
+  },
+  refuse(e) {
+    let that = this;
+    let index = e.currentTarget.dataset.index;
+    this.data.applications.forEach((item, _) => {
+      if (item.OpenId == index) {        
+        wx.showModal({
+          title: '确认拒绝以下申请吗',
+          content: item.nickName +'申请' + item.community,
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              //发送后端 TODO
+              that.confirmRefuse(e);
+            } else if (res.cancel) {
+              console.log('用户点击取消');
+              // 在这里执行取消操作
+            }
+          }
+        })
+        return;
+      }
+    });
+  },
+  deletOneContact(e) {
+    if (this.data.myContacts.length == 1) {
       wx.showToast({
         title: "至少保留一条联系方式",
         icon: "error"
@@ -25,33 +99,41 @@ Page({
       }
     });
     this.setData({
-      myContacts:community
+      myContacts: community
     })
   },
-  deletContact(e){
+  deletContact(e) {
     let that = this;
-    wx.showModal({
-      title: '退出该联系方式吗',
-      content: '',
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定');
-          that.deletOneContact(e);
-        } else if (res.cancel) {
-          console.log('用户点击取消');
-          // 在这里执行取消操作
-        }
+    let index = e.currentTarget.dataset.index;
+    this.data.myContacts.forEach((item, i) => {
+      if (i == index) {
+        console.log(item);
+        wx.showModal({
+          title: '删除该联系方式吗',
+          content: item,
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              that.deletOneContact(e);
+            } else if (res.cancel) {
+              console.log('用户点击取消');
+              // 在这里执行取消操作
+            }
+          }
+        })
+        return;
       }
-    })
+    });
+    
   },
-  deletOneCommunity(e) {  
-    if(this.data.myCommunity.length == 1){
+  deletOneCommunity(e) {
+    if (this.data.myCommunity.length == 1) {
       wx.showToast({
         title: "至少在一个小区",
         icon: "error"
       });
       return;
-    }  
+    }
     let index = e.currentTarget.dataset.index;
     let community = [] as string[];
     this.data.myCommunity.forEach((item, i) => {
@@ -61,24 +143,32 @@ Page({
       }
     });
     this.setData({
-      myCommunity:community
+      myCommunity: community
     })
   },
-  deletCommunity(e){
+  deletCommunity(e) {
     let that = this;
-    wx.showModal({
-      title: '退出该小区吗',
-      content: '',
-      success: function (res) {
-        if (res.confirm) {
-          console.log('用户点击确定');
-          that.deletOneCommunity(e);
-        } else if (res.cancel) {
-          console.log('用户点击取消');
-          // 在这里执行取消操作
-        }
+    let index = e.currentTarget.dataset.index;
+    this.data.myCommunity.forEach((item, i) => {
+      if (i == index) {
+        console.log(item);
+        wx.showModal({
+          title: '退出该小区吗',
+          content: item,
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定');
+              that.deletOneCommunity(e);
+            } else if (res.cancel) {
+              console.log('用户点击取消');
+              // 在这里执行取消操作
+            }
+          }
+        })
+        return;
       }
-    })
+    });
+    
   },
   /**
    * 生命周期函数--监听页面加载
